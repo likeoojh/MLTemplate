c22f835c409b7de51718c178ee64d48cc6f68c8e
RektPunk <rektpunk@gmail.com>
2022-08-07 16:57:32 +0900

    feat - add read logic in read data

--- module/utils/read_data.py
+++ module/utils/read_data.py
@@ -21,28 +21,53 @@ def _read_check(
     """
     os.makedirs(cache_path, exist_ok=True)
     try:
-        with open(f"{cache_path/cached_filename}", "rb") as rfp:
+        with open(f"{cache_path}/{cached_filename}", "rb") as rfp:
             df = pickle.load(rfp)
         return df
     except:
         return None
 
 
+def _read_logic(
+    read_inputs: Dict,
+    cached_filename: str,
+    cache_path: str = ".cache/",
+) -> Dict[str, pd.DataFrame]:
+    """
+    Read csv 
+    Args:
+        read_inputs (Dict): {"df_name": df_paths, ...}
+        cached_filename (str)
+        cache_path (str)
+    Returns:
+        Dict[str, pd.DataFrame]: {"df_name": pd.DataFrame, ...}
+    """
+    df_dict = {}
+    for key, item in read_inputs.items():
+        df_dict.update({key: pd.concat([pd.read_csv(_) for _ in item])})
+    with open(f"{cache_path}/{cached_filename}", "wb") as wfp:
+        pickle.dump(df_dict, wfp)
+    return df_dict
+
+
 def read_data(
-    inputs: Dict,
-) -> pd.DataFrame:
+    read_inputs: Dict,
+) -> Dict[str, pd.DataFrame]:
     """
     Load data example
     Args:
         inputs (Dict): inputs
     Return:
-        pd.DataFrame: data
+        Dict[str, pd.DataFrame]: data dictionary
     """
-    _inputs_hash = object_hash(inputs)
+    _inputs_hash = object_hash(read_inputs)
     _cached_filename = f"{_inputs_hash}.pickle"
-    df = _read_check(_cached_filename)
-    if df is None:
-        ## read logic
-        return df
+    df_dict = _read_check(_cached_filename)
+    if df_dict is None:
+        df_dict = _read_logic(
+            read_inputs,
+            _cached_filename,
+        )
+        return df_dict
     else:
-        return df
+        return df_dict
