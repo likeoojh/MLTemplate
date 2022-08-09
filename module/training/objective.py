a4c2dc407641d7c90fcd9ff7a50ca352fc4069bd
RektPunk <rektpunk@gmail.com>
2022-08-09 16:26:06 +0900

    feat - add optuna objective

---
+++ module/training/objective.py
@@ -0,0 +1,44 @@
+import lightgbm as lgb
+import pandas as pd
+import numpy as np
+
+
+def optuna_objective(
+    trial,
+    train_x: pd.DataFrame,
+    train_y: pd.Series,
+    valid_x: pd.DataFrame,
+    valid_y: pd.Series,
+) -> float:
+    """
+    optuna objective function
+    Args:
+        trial
+        train_x (pd.DataFrame):
+        train_y (pd.Series):
+        valid_x (pd.DataFrame):
+        valid_y (pd.Series):
+        weight_train (pd.Series):
+
+    Returns:
+        float: rmse score
+    """
+    dtrain = lgb.Dataset(data=train_x, label=train_y)
+    param = {
+        "objective": "rmse",
+        "metric": "rmse",
+        "verbosity": -1,
+        "boosting_type": "gbdt",
+        "lambda_l1": trial.suggest_float("lambda_l1", 1e-8, 10.0, log=True),
+        "lambda_l2": trial.suggest_float("lambda_l2", 1e-8, 10.0, log=True),
+        "num_leaves": trial.suggest_int("num_leaves", 2, 256),
+        "feature_fraction": trial.suggest_float("feature_fraction", 0.4, 1.0),
+        "bagging_fraction": trial.suggest_float("bagging_fraction", 0.4, 1.0),
+        "bagging_freq": trial.suggest_int("bagging_freq", 1, 7),
+        "min_child_samples": trial.suggest_int("min_child_samples", 5, 100),
+    }
+
+    gbm = lgb.train(param, dtrain)
+    preds = gbm.predict(valid_x)
+    rmse = np.sqrt(np.mean((valid_y - preds) ** 2))
+    return rmse
